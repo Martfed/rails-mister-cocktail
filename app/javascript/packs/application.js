7f4c62228d0dc33a5ae574359df7d508e8c5c0e7
Federico De Martino <federicodm90@gmail.com>
2019-08-09 16:27:45 +0200

    with some Js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,7 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import 'bootstrap';
+
 console.log('Hello World from Webpacker')
 
 $(document).ready(function() {
@@ -29,4 +30,3 @@ $(".banner").mousemove(function(e){
           $('.banner').css("background-position", newvalueX+"px     "+newvalueY+"px");
 });
 });
-
